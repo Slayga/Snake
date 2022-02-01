e740c562fc786188ab859deaeca1ded59fb7f7f7
Slayga <gabbe_03_02@hotmail.se>
2022-02-01 23:26:03 +0100

    Head change

--- Snake/classes/berry.py
+++ Snake/classes/berry.py
@@ -5,9 +5,9 @@ Info:
 Berry Module
 """
 from tkinter import Label, N, S, W, E
-from snakeModule import Snake
-from boardModule import GameBoard
-from tailModule import Tail
+from head import Head
+from board import GameBoard
+from tail import Tail
 from random import randint
 
 
@@ -23,22 +23,22 @@ class Berry:
     def run(self):
         ...
 
-    def grid(self, board: GameBoard, snake: Snake, tail: Tail):
+    def grid(self, board: GameBoard, snake: Head, tail: Tail):
         lbl = board.get_lbls()
         while True:
-            self.x, self.y = randint(board.x_min,
-                        board.x_max - 1), \
-                    randint(board.y_min,
-                            board.y_max - 1)
+            self.x, self.y = randint(board.x_min, board.x_max - 1), randint(
+                board.y_min, board.y_max - 1
+            )
 
-            if (lbl.get(self.y)[self.x].cget("bg") != tail.color and \
-                (self.x, self.y) != (snake.x, snake.y)):
-                self.lbl_berry.grid(column=self.x,
-                                    row=self.y,
-                                    sticky=N + S + W + E)
+            if lbl.get(self.y)[self.x].cget("bg") != tail.color and (
+                self.x,
+                self.y,
+            ) != (snake.x, snake.y):
+                self.lbl_berry.grid(
+                    column=self.x, row=self.y, sticky=N + S + W + E
+                )
                 return
             else:
-                self.x, self.y = randint(board.x_min,
-                        board.x_max - 1), \
-                    randint(board.y_min,
-                            board.y_max - 1)
+                self.x, self.y = randint(
+                    board.x_min, board.x_max - 1
+                ), randint(board.y_min, board.y_max - 1)
